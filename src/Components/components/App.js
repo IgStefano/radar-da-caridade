4fb8ac658cf9b5e02d11dbf7424e7c786eadfea8
Igor Stefano <igstefano90@live.com>
2021-11-21 18:02:53 -0300

    add edit e delete

--- src/Components/components/App.js
+++ src/Components/components/App.js
@@ -3,6 +3,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Sobre from "./Sobre";
 import Cadastro from "./Cadastro";
 import Home from "./Home";
+import AçãoÚnica from "./AçãoÚnica";
+import EditarAção from "./EditarAção";
+import DeletarAção from "./DeletarAção";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Sobrecss from "../../Assets/Styles/Sobre.css";
 
 function App() {
@@ -13,6 +17,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Sobre" element={<Sobre />} />
           <Route path="/Cadastro" element={<Cadastro />} />
+          <Route path="/:id" element={<AçãoÚnica />} />
+          <Route path="/editar/:id" element={<EditarAção />} />
+          <Route path="/deletar/:id" element={<DeletarAção />} />
         </Routes>
       </div>
     </div>
