a78613d8f1fcb1a2b43484116b0b8439cb5cde86
Michael Sales <michael.fernandes.sales@gmail.com>
2021-11-22 23:10:07 -0300

    Estilizei a home

--- src/Components/components/Home.js
+++ src/Components/components/Home.js
@@ -7,33 +7,55 @@ import MapView from "./MapView";
 
 function Home() {
   return (
-    <div className="background">
-      <h1 className="TitleR">Radar</h1>
-      <h1 className="TitleD">da</h1>
-      <h1 className="TitleC">Caridade</h1>
-      <img src={Logo} alt="Radar da Caridade" className="LogoHome" />
+    <div>
+      <div className="background">
+        <h1 className="TitleR">Radar</h1>
+        <h1 className="TitleD">da</h1>
+        <h1 className="TitleC">Caridade</h1>
+        <img src={Logo} alt="Radar da Caridade" className="LogoHome" />
 
-      <div className="Links">
-        <Link to="/Sobre">
-          <h2 className="sobre">Conheça mais sobre o projeto</h2>
-        </Link>
-        <Link to="/Cadastro">
-          <h2 className="cadastro">Cadastre aqui as suas ações</h2>
-        </Link>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="row"></div>
+        <div className="Links">
+          <Link to="/Sobre">
+            <h2
+              className="sobre btn btn-success"
+              style={{
+                height: "8%",
+                width: "28%",
+                fontSize: "25px",
+                color: "white",
+              }}
+            >
+              Conheça mais sobre o projeto
+            </h2>
+          </Link>
+          <Link to="/Cadastro">
+            <h2
+              className="cadastro btn btn-success"
+              style={{
+                height: "9%",
+                width: "28%",
+                fontSize: "25px",
+                color: "white",
+              }}
+            >
+              Cadastre aqui as suas ações
+            </h2>
+          </Link>
         </div>
-        <div className="row">
-          <div
-            style={{ height: "400px", overflowX: "hidden" }}
-            className="overflow-scroll w-25 col-2"
-          >
-            <Lista />
+        <div className="container">
+          <div className="row">
+            <div className="row"></div>
           </div>
-          <div className="col-8">
-            <MapView />
+          <div className="row">
+            <div
+              style={{ height: "400px", overflowX: "hidden" }}
+              className="overflow-scroll w-25 col-2"
+            >
+              <Lista />
+            </div>
+            <div className="col-8">
+              <MapView />
+            </div>
           </div>
         </div>
       </div>
